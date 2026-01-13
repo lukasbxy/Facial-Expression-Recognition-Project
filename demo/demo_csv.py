fc2415a98353056abc6c825e3b6eb56365f39ebf
kaiuu75 <ksavas@evimiz.net>
2026-01-13 17:35:25 +0100

    cleaned up files

--- demo/demo_csv.py
+++ demo/demo_csv.py
@@ -51,10 +51,7 @@ def load_model(model_weights,device):
     return model 
 
 def classify_image(model, image_path,device):
-    transform = transforms.Compose([
-        transforms.Resize((64,64)),
-        transforms.ToTensor(),
-    ])
+    transform = transforms.ToTensor()
     image = Image.open(image_path).convert("RGB")
     img_tensor = transform(image).unsqueeze(0).to(device)
 
