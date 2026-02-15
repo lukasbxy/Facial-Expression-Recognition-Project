a0dc3e6d8da4ecb430cd88fbfaa97476291f6226
Paul T. <onecrafterlp@gmail.com>
2026-02-15 19:43:26 +0100

    Add Toggle for Weighted Random Sampler to main and training script

--- training/ResNetTrainer.py
+++ training/ResNetTrainer.py
@@ -45,7 +45,9 @@ class ResNetTrainer:
                  class_limit: int = None,
                  best_model_filename: str = "best.pt",
                  last_model_filename: str = "last.pt",
-                 early_stopping_patience: int = 5):
+                 early_stopping_patience: int = 5,
+                 use_weighted_sampler: bool = True,
+                 weight_power: float = 1.0):
         self.num_epochs = num_epochs
         self.learning_rate = learning_rate
         self.weight_decay = weight_decay
@@ -58,6 +60,8 @@ class ResNetTrainer:
         self.best_model_filename = best_model_filename
         self.last_model_filename = last_model_filename
         self.early_stopping_patience = early_stopping_patience
+        self.use_weighted_sampler = use_weighted_sampler
+        self.weight_power = weight_power
         
         # Store model name and timestamp for this run.
         self.model_name = model.__class__.__name__
@@ -88,7 +92,9 @@ class ResNetTrainer:
         self.train_loader, self.val_loader, self.class_names = get_dataloaders(
             train_datasets=self.train_datasets, 
             val_datasets=self.val_datasets,
-            class_limit=self.class_limit
+            class_limit=self.class_limit, 
+            use_weighted_sampler=self.use_weighted_sampler,
+            weight_power=self.weight_power
         )
         
         # Compute class weights if needed
@@ -250,7 +256,9 @@ class ResNetTrainer:
             "model.class": self.model_name,
             "scheduler": self.scheduler.__class__.__name__ if self.use_scheduler else None,
             "optimizer": self.optimizer.__class__.__name__,
-            "early_stopping_patience": self.early_stopping_patience
+            "early_stopping_patience": self.early_stopping_patience,
+            "use_weighted_sampler": self.use_weighted_sampler,
+            "weight_power": self.weight_power
         }
         
     def train_one_epoch(self, epoch):
